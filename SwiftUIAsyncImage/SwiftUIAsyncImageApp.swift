f1a4307d7ae0c3151dd9b24d77e646b6c0dd19b7
Yashwant Das <yashworks@gmail.com>
2022-01-28 17:14:03 +0530

    Initial Commit

---
+++ SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Yashwant Das on 28/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
